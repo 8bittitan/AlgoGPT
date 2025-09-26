cca394759690d1ead883570a19f55994b113d94e
Paul Jankowski <8BitTitan@gmail.com>
2025-09-26 13:02:22 -0400

    remove set constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const API_URL = "https://askai.algolia.com/chat";
-export const INDEX = "docsearch-markdown";
-export const APP_ID = "PMZUYBQDAK";
-export const SEARCH_API_KEY = "a00716d83c64f6c61905c078b7d5ab66";
-export const ASSISTANT_ID = "askAIDemo";
+export const INDEX = "";
+export const APP_ID = "";
+export const SEARCH_API_KEY = "";
+export const ASSISTANT_ID = "";
